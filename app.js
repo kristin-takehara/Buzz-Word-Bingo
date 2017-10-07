fc59150c390d6b942550eeddbfce2b6d57e5098b
Kristin Takehara <KRISTINTAKEHARA@Kristins-MacBook-Pro.local>
2017-10-06 19:10:13 -1000

    this read me is blah

--- app.js
+++ app.js
@@ -9,19 +9,26 @@ app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
-  res.send('hi hi');
-});
-
-app.get('/buzzwords', (req, res) => {
   res.send();
 });
 
-app.post('/buzzword', (req, res) => {
+app.get('/buzzwords', (req, res) => {
+  let buzzwords = { "buzzwords": [] };
+  for (var i = 0; i < buzzwords.length; i++) {
+
+  }
   res.send(200, {
     'Content-Type': 'application/json',
-    'success': 'true'
+    'success': true
   });
 });
+
+// app.post('/buzzword', (req, res) => {
+//   res.send(200, {
+//     'Content-Type': 'application/json',
+//     'success': true
+//   });
+// });
 
 app.listen(3000, function () {
   console.log('Buzz-Word-Bingo listening on port 3000!');
