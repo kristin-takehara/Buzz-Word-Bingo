f70356aa397519cd6bb6efa754396a33fa1a422c
Kristin Takehara <KRISTINTAKEHARA@Kristins-MacBook-Pro.local>
2017-10-07 14:03:44 -1000

    server working. browser connecting. basic function in place

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const buzzwords = require('./buzzword.js');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+
 app.use('/', buzzwords); //serve index.html (located in the public directory)
 
-app.listen(3000);
+app.listen(3000, () => {
   console.log('Buzz-Word-Bingo listening on port 3000!');
+});
